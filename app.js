7d75102a43423e8af7d1d28333d5754e2e718d3d
bekuh-esfira <bekbolsun69.69@gmail.com>
2024-11-08 15:25:02 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+// //условные операторы
+// //if, else,switch,ternnary//
+
+
+// // const num = +prompt("write a number");
+// // const num2 = +prompt("write a number again");
+
+// // if (num < num2) {
+// //   console.log(`${num2} > ${num}`);
+// // } else if (num > num2) {
+// //   console.log(`${num} > ${num2}`);
+// // } else if (num == num2) {
+// //   console.log(`${num} == ${num2}`);
+// // } else {
+// //   console.log("error");
+// // }
+
+// const num =+prompt("7day")
+// switch (num) {
+//     case ("1"):
+//     alert("понедельник");
+//     break;
+
+//     case "2":
+//     alert("вторник");
+//     break;
+
+//     case "3":
+//     alert("среда");
+//     break;
+
+//     case "4":
+//     alert("чутверг");
+//     break;
+
+//     case "5":
+//     alert("пятница");
+//     break;
+ 
+//     case "6":
+//     alert("суббота");
+//     break;
+
+//     case "7":
+//     alert("воскресенье");
+//     break;
+//     default:
+//         alert("башка жок!")
+// }
+
+// 1 тапшырмa
+
+// const num =+prompt("кун суук!")
+// const num2 =+prompt("кун жылуу!")
+// const num3 =+prompt ("кун ысык!")
+// if (num < num2 <num3) { console.log ()}
+
+
+//3 тапшырма!
+// const num =+prompt("12 ай мезгили!")
+
+// switch (num ) {
+//     case 2:
+//     case 12:
+//         case 1:    
+//     alert("кыш мезгили ");
+//     break;
+
+//     case 3:
+//     case 4:
+//     case 5: 
+//     alert("жаз мезгили");
+//     break;
+
+//     case 6:
+//     case 7:
+//     case 8:
+//     alert("жай мезгили");
+//     break;
+
+//     case 9:
+//     case 10:
+//     case 11:
+//     alert("куз мезгили!");
+//     break;
+//     default:
+//     alert("башка жок!")
+// }
+const num2 =+prompt("12 ай мезгили!")
+
+switch (num2 ) {
+    case 2:
+    case 12:
+    case 1:    
+    console.log("кыш мезгили ");
+    break;
+
+    case 3:
+    case 4:
+    case 5: 
+    console.log("жаз мезгили");
+    break;
+
+    case 6:
+    case 7:
+    case 8:
+    console.log("жай мезгили");
+    break;
+
+    case 9:
+    case 10:
+    case 11:
+    console.log("куз мезгили!");
+    break;
+    default:
+    console.log("башка жок!")
+}
+
+
+
+  
+
